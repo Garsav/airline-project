1230ba449747d56a27a7c80f02ed34d01b053dbc
Garsav <135902417+Garsav@users.noreply.github.com>
2025-09-29 14:18:08 -0500

    Update FlightRepository.java

--- api-spring/src/main/java/com/garrett/airline/repo/FlightRepository.java
+++ api-spring/src/main/java/com/garrett/airline/repo/FlightRepository.java
@@ -1,2 +1,13 @@
 package com.garrett.airline.repo;
 
+import com.garrett.airline.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
+    List<Flight> findByOrigin(String origin);
+    List<Flight> findByDestination(String destination);
+}
+
+
