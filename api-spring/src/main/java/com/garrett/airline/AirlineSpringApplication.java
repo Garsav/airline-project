cc9e7bbb5017afe15f820a52a3ce175cab1b9e10
Garsav <135902417+Garsav@users.noreply.github.com>
2025-09-29 11:36:02 -0500

    Update AirlineSpringApplication.java

--- api-spring/src/main/java/com/garrett/airline/AirlineSpringApplication.java
+++ api-spring/src/main/java/com/garrett/airline/AirlineSpringApplication.java
@@ -1,4 +1,4 @@
-package com.garrett.airline_spring;
+package com.garrett.airline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
